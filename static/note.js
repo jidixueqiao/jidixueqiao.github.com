d422aab26f565857add5312d943122e8a185de4f
jidixueqiao <2043832968@qq.com>
2023-12-05 22:17:29 +0800

    modify noteMenu

--- static/note.js
+++ static/note.js
@@ -1,4 +1,20 @@
-const noteMenu = []
+const noteMenu = [
+  {
+    id: 1,
+    name: 'JavaScript',
+    children: '',
+  },
+  {
+    id: 2,
+    name: 'Rust',
+    children: '',
+  },
+  {
+    id: 3,
+    name: 'Tauri',
+    children: '',
+  },
+]
 
 export default {
   noteMenu,
