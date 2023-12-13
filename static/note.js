8ef615fcf5e6d47cc1158e0052ffbdb11ecd78e5
jidixueqiao <2043832968@qq.com>
2023-12-13 20:24:23 +0800

    2023-12-13 updadte: 1.add a test file 2.add an essay data

--- static/note.js
+++ static/note.js
@@ -2,7 +2,13 @@ const noteMenu = [
   {
     id: 1,
     name: 'JavaScript',
-    children: '',
+    children: [
+      {
+        title: '测试文章',
+        updateTime: '2023-12-13 20:23',
+        content: './note/javascript/test.json',
+      },
+    ],
   },
   {
     id: 2,
